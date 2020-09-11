36362c67725103a105e5795710cb02922c2f3b0a
dowhile26 <n.white001@yandex.ru>
2020-09-11 11:57:24 +0300

    Add lib files

---
+++ lib/components/round_icon_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import '../constants.dart';
+
+class RoundIconButton extends StatelessWidget {
+  RoundIconButton({this.icon, this.onPressed});
+
+  final IconData icon;
+  final Function onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      shape: CircleBorder(),
+      fillColor: kButtonColor,
+      constraints: BoxConstraints.tightFor(
+        width: 56.0,
+        height: 56.0,
+      ),
+      elevation: 6.0,
+      onPressed: onPressed,
+      child: Icon(icon),
+    );
+  }
+}
